fc3279734690fa62817a41239484f68752544fbb
jesiryan <jesiryan@gmail.com>
2014-03-31 15:26:08 +0100

    just in case it breaks when a new jboss project runs

--- TrialConnectionToDatabase/src/testing/FailureClassServiceEJBTest.java
+++ TrialConnectionToDatabase/src/testing/FailureClassServiceEJBTest.java
@@ -12,16 +12,16 @@ public class FailureClassServiceEJBTest {
 //	public final void testSetDao() {
 //		fail("Not yet implemented");
 //	}
-
+	
 	@Test
 	public final void testGetFailureClasses() {
 		FailureClassServiceEJB fcsejb = new servicesEJB.FailureClassServiceEJB();
 		assertEquals(fcsejb.getFailureClasses().get(0).getCallFailures().get(0).getClass(), entities.CallFailure.class);
 	}
-
+	
 //	@Test
 //	public final void testAddToFailureClasses() {
 //		fail("Not yet implemented");
 //	}
-
+	
 }
